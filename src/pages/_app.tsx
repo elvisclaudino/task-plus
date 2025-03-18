d2ffa746366cc6d9c13f28778685eaa67428f73b
Elvis Claudino <102040112+elvisclaudino@users.noreply.github.com>
2025-03-18 20:33:47 -0300

    feat: add google login

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,15 @@
-import { Header } from "@/components/header";
-import "@/styles/globals.css";
+import { SessionProvider } from "next-auth/react";
 import type { AppProps } from "next/app";
+
+import "@/styles/globals.css";
+
+import { Header } from "@/components/header";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <SessionProvider session={pageProps.session}>
       <Header />
       <Component {...pageProps} />
-    </>
+    </SessionProvider>
   );
 }
